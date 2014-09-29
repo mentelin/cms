ebc1040e5c44eaa1f98dc771494b8d8877a94bb8
Vadim Yalo <vadim@Vadims-MacBook-Pro.local>
2014-09-29 22:02:40 +0400

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cmsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
